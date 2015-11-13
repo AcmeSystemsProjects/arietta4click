435ddc65a23caa350e50af26803c92612693f3a1
Sergio Tanzilli <tanzilli@acmesystems.it>
2015-11-13 23:58:23 +0100

    Example

--- relay_click.py
+++ relay_click.py
@@ -4,19 +4,15 @@ import time
 print "RELAY click example"
 print "Type ctrl-C to exit"
 
-#Use mikroBUS slot #1
 mikroBUS = 1 
 
 REL1 = clickboard.RelayClick("REL1",mikroBUS)
 REL2 = clickboard.RelayClick("REL2",mikroBUS)
  
-while True:
-	time.sleep(1)
+REL1.on()
+REL2.on()
 
-	REL1.on()
-	REL2.off()
-	
-	time.sleep(1)
-	
-	REL1.off()
-	REL2.on()
+time.sleep(1)
+
+REL1.off()
+REL2.off()
