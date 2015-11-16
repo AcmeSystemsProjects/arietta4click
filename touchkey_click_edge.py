fe50001aff78be6574ac85d1064c6cc82b24e1c0
Sergio Tanzilli <tanzilli@acmesystems.it>
2015-11-16 23:58:30 +0100

    Touchkey click example working with interrupt

--- touchkey_click_edge.py
+++ touchkey_click_edge.py
@@ -9,6 +9,7 @@
 #
 
 import clickboard
+import time
  
 # Touchkey click on slot 1
 
@@ -22,13 +23,18 @@ KeyD = clickboard.TouchClick("D",1)
 REL1 = clickboard.RelayClick("REL1",2)
 REL2 = clickboard.RelayClick("REL2",2)
 
-def timedREL1(secs=1):
+def timedREL1():
 	REL1.on()
-	time.sleep(secs)
+	time.sleep(1)
 	REL1.off()
-	
 
-KeyA.set_edge("rising",timedREL1())
+def timedREL2():
+	REL2.on()
+	time.sleep(1)
+	REL2.off()
+	
+KeyA.set_edge("rising",timedREL1,0)
+KeyB.set_edge("rising",timedREL2,0)
 
 while True:
-	time.sleep(20)
+	time.sleep(1)
