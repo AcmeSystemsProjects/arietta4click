b59485a6b69c5e82bd7b9c057bd1c58745fae3fb
Sergio Tanzilli <tanzilli@acmesystems.it>
2015-11-16 16:21:43 +0100

    relay_click

---
+++ relay_click_bus2.py
@@ -0,0 +1,20 @@
+#Turn-on both relays for one second 
+
+import clickboard
+import time
+ 
+print "RELAY click example"
+print "Type ctrl-C to exit"
+
+mikroBUS = 2
+
+REL1 = clickboard.RelayClick("REL1",mikroBUS)
+REL2 = clickboard.RelayClick("REL2",mikroBUS)
+ 
+REL1.on()
+REL2.on()
+
+time.sleep(1)
+
+REL1.off()
+REL2.off()
